51e9a56dfd0c0f1306064f622d2eaa38ae2c69eb
AkashJana18 <akashjana663@gmail.com>
2025-01-18 18:57:11 +0530

    Backend: Role base check

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+// Define public routes
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/public(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  // Allow access to public routes without authentication
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
